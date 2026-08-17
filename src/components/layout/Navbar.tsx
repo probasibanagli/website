17c17fe27c22b50129ce0156afdef28af0cd2210
Balaji R <155892613+Balaji2327@users.noreply.github.com>
2026-08-17 11:26:09 +0530

    The gaps are reduced

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -105,13 +105,13 @@ export function Navbar() {
 
             {/* Desktop Nav */}
             <div className="hidden lg:flex items-center gap-1">
-              <Link
+              {/* <Link
                 href="/"
                 onClick={(e) => handleLinkClick(e, '/')}
                 className="px-3 py-2 text-sm font-medium text-text-primary hover:text-primary transition-colors rounded-lg hover:bg-surface"
               >
                 <T>Home</T>
-              </Link>
+              </Link> */}
               {navLinks.map((link) => (
                 <div
                   key={link.label}
